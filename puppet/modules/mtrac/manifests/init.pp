42adb471e06317f88234abbcbd1e587e5650a14e
Chris Ford <christophertford@gmail.com>
2012-11-27 17:33:01 +0530

    Install Django modules.

--- puppet/modules/mtrac/manifests/init.pp
+++ puppet/modules/mtrac/manifests/init.pp
@@ -2,4 +2,13 @@ class mtrac {
   package { "python-pip":
     ensure => present,
   }
+
+  $django = ["django", "django-extensions", "django-nose", "django-tablib",
+             "django-uni-form", "djappsettings", "django-mptt", "django-guardian",
+             "django-digest", "django-celery"]
+
+  package { $django:
+    ensure => present,
+    provider => pip,
+  }
 }
